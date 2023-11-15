195968380bb35a826e1dda4da1fb1cfb3860b7a6
TeemuHarju <harju9797@gmail.com>
2023-11-15 21:58:24 +0200

    Added fmt.

---
+++ vcpkg_bootstrap.cmake
@@ -0,0 +1,19 @@
+# vcpkg_bootstrap.cmake
+
+# Download and install vcpkg
+execute_process(
+  COMMAND git submodule update --init
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+# Bootstrap vcpkg
+execute_process(
+  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/bootstrap-vcpkg.bat
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+# Install dependencies
+execute_process(
+  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/vcpkg install --triplet x64-windows
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
