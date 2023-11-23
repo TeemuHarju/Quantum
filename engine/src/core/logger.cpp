86f9c9c5185dec27d59f297f0f6ab4b703155286
Teemu Harju <harju9797@gmail.com>
2023-11-23 22:40:46 +0200

    Removed everything.

--- engine/src/core/logger.cpp
+++
@@ -1,7 +0,0 @@
-#include "logger.h"
-
-void Logger::initialize() {
-	spdlog::set_pattern("[%H:%M:%S.%e] [%^%L%$] [thread %t] [%s:%#] %v");
-	console_logger = spdlog::stdout_color_mt("console");
-	console_logger->set_level(spdlog::level::trace);
-}
